2c642efb50d5434f4baca4766c79b78d229f405d
Patrik Henningsson <patrik.henningsson@gmail.com>
2017-11-14 00:09:10 +0100

    Use correct colors for JSX tag

---
+++ example.js
@@ -0,0 +1,15 @@
+const Button = props => {
+  const { kind, ...other } = props;
+  const className = kind === "primary" ? "PrimaryButton" : "SecondaryButton";
+  return <button className={className} {...other} />;
+};
+
+const App = () => {
+  return (
+    <div>
+      <Button kind="primary" onClick={() => console.log("clicked!")}>
+        Hello World!
+      </Button>
+    </div>
+  );
+};
